fb118704f0e39e1d99ed6aa2b4a7b4f5a28a7dd9
Brian <brian@hamroute.com>
2011-08-02 06:55:47 +0000

    Changing the copyright text

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -50,7 +50,7 @@
 	<?php echo $content; ?>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by hamroute.com.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
