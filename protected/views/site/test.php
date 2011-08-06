dfbcbad38f89581ec4e41683fa7f0691ebc148cf
Reuben <reuben@hamroute.com>
2011-08-06 17:21:40 +0000

    added some UI to the test

--- protected/views/site/test.php
+++ protected/views/site/test.php
@@ -27,7 +27,7 @@ Yii::app()->clientScript->registerScript(
 				$('<span/>', {'class':'start', text: info.start}).appendTo(main);
 				$('<span/>', {text:' to '}).appendTo(main);
 				$('<span/>', {'class':'end', text: info.end}).appendTo(main);
-
+        
 				main.click(function(){
 					// Change our map
 					$('#repeaters').map('drawRoute', info.start, info.end);
@@ -85,13 +85,60 @@ CClientScript::POS_HEAD);
 
 ?>
 
-<div id="map_canvas" style="width:100%; height:400px;">map_canvas</div>
+<!--<div id="map_canvas" style="width:100%; height:400px;">map_canvas</div>-->
 <div id="trigger">directions</div>
 <div style="float:right;">
 	<h3 style="margin-bottom: 5px;">Recent Searches</h3>
 	<div id="recentSearches">Loading...</div>
 </div>
 <div id="repeaters"></div>
+<script type="text/javascript">
+  function DivSwitch(obj,newdiv){
+  if(document.getElementById){
+  var el = document.getElementById(obj);
+  var ar = document.getElementById(newdiv).getElementsByTagName('div');
+  var def = document.getElementById('defaultDiv');
+  if(el.style.display == 'none'){
+  for (var i=0; i<ar.length; i=""
+				ar=""[i=""].style.display = 'none'
+			}
+			el.style.display = 'block'
+		}else=''
+			el.style.display = ''
+			def.style.display = 'block';
+		}
+	}
+}
+</script>
+
+<div style="position:absolute; left:20px; top:20px;">
+  <form>
+    Start Location: <input type="text" name="fromLocation" id="FromLocation" style="width:220px;" />
+    Destination: <input type="text" name="toLocation" id="ToLocation" style="width:220px;" />
+    <input type="submit" value="Search" />
+  </form>
+</div>
+
+<div style="position:absolute; left:20px; top:50px; background:url('images/HAM-UI_04.png'); width:179px; height:286px;">
+  recent searches
+</div>
+
+<div id="map_canvas" style="position:absolute; width:424px; height:286px; left:200px; top:50px; background:url('images/HAM-UI_05.png');">
+  map canvas
+</div>
+
+<div style="position:absolute; left:20px; top:350px; width:603px; height:434px; background:url('images/HAM-UI_06.png') no-repeat center;">
+  <div style="position:absolute; top:0px; left:70px; font-weight:bold;">
+    <a href="javascript:DivSwitch('Repeaters','folder');">Repeaters</a>
+  </div>
+  <div style="position:absolute; top:0px; left:195px; font-weight:bold;">
+    <a href="javascript:DivSwitch('Directions','folder');">Directions</a>
+  </div>
+  <div id="folder" style="position:relative; top:50px; left:25px;">
+    <div id="Repeaters">Repeaters List</div>
+    <div id="Directions" style="display:none;">Directions</div>
+  </div>
+</div>
 <?php
 foreach($repeaters as $key => $repeater) {
 	//echo "<div onclick=\"$().map('toggleRepeater', {$key});\">toggle {$key}</div>";
